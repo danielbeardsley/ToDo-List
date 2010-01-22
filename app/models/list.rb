68a389e76972a5fe4d440c0258f748bd5f1eb9e8
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-01-22 01:01:13 -0800

    Added a few methods to choose and set the current_item for a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,18 @@
 class List < ActiveRecord::Base
-  has_many :items
-  belongs_to :current_item, :class_name => 'item'
+  has_many :items do
+    def next
+      proxy_target.to_a.rand
+    end
+  end
+  belongs_to :current_item, :class_name => 'Item'
+
+  def get_current_item
+    current_item || choose_next_item
+  end
+
+  def choose_next_item
+    self.current_item = items.next
+    self.current_item.update_attribute(:last_seen, Time.now) if self.current_item
+    self.current_item
+  end
 end
--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -3,4 +3,34 @@ require 'test_helper'
 class ListTest < ActiveSupport::TestCase
   should_have_many :items
   should_belong_to :current_item
+
+  context "A List with several items" do
+    setup do
+      @list = List.create(:name => 'List1')
+      @items = (1..3).to_a.map do |i|
+        @list.items.create(:title => "Item #{i}")
+      end
+    end
+
+    should "set current_item to one of it's items" do
+      assert_nil @list.current_item
+
+      @list.choose_next_item
+      assert_contains @items, @list.current_item
+
+      assert @list.current_item.last_seen > 5.seconds.ago
+    end
+
+    should "set current_item when get_current_item is first called" do
+      assert_nil @list.current_item
+
+      item = @list.get_current_item
+      assert_equal item, @list.current_item
+      assert_contains @items, @list.current_item
+    end
+
+    should "return an appropriate item to do" do
+      assert_contains @items, @list.items.next
+    end
+  end
 end
